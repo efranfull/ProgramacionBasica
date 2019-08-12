b4a148d2fdef0ce8fa18b98efe5ba724772f7921
Ramon Guzman <efranfull@gmail.com>
2019-08-12 15:36:50 -0400

    Funciones en JavaScript
    La funciones son una herramienta que nos permite escribir código que vamos a re-usar múltiples veces.
    Puedes escribir una función en JavaScript así:
    function nombreFuncion(parametros)
    {
     //Código que ejecuta la función
    }

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,21 +1,18 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
-lienzo.beginPath(); /*Funcion que inicia el dibujo*/
-lienzo.strokeStyle = "blue"; /*atrubuto o propierda del lienzo 2d "d.getContext(2d)"*/
-lienzo.moveTo(50, 10); /*Para mover la posicion del lienzo*/
-lienzo.lineTo(250, 2); /*Para trazar una linea*/
-lienzo.stroke(); /*Es el hecho de trazar la raya*/
-lienzo.closePath(); /* Funcion que cierra el dibujo*/
+dibujarLinea("pink", 10, 300, 220, 10);
+dibujarLinea("yellow", 300, 10, 10, 220);
 
-/* Para hacer una segunda linea*/
-
-lienzo.beginPath(); /*Funcion que inicia el dibujo*/
-lienzo.strokeStyle = "red"; /*atrubuto o propierda del lienzo 2d "d.getContext(2d)"*/
-lienzo.moveTo(180, 10); /*Para mover la posicion del lienzo*/
-lienzo.lineTo(290, 200); /*Para trazar una linea*/
-lienzo.stroke(); /*Es el hecho de trazar la raya*/
-lienzo.closePath(); /* Funcion que cierra el dibujo*/
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+  lienzo.beginPath(); /*Funcion que inicia el dibujo*/
+  lienzo.strokeStyle = color; /*atrubuto o propierda del lienzo 2d "d.getContext(2d)"*/
+  lienzo.moveTo(xinicial, yinicial); /*Para mover la posicion del lienzo*/
+  lienzo.lineTo(xfinal, yfinal); /*Para trazar una linea*/
+  lienzo.stroke(); /*Es el hecho de trazar la raya*/
+  lienzo.closePath(); /* Funcion que cierra el dibujo*/
+}
 
 
 
