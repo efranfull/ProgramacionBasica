5664aacc833a2d5f40b109dc10a88ad106e5e254
Ramon Guzman <efranfull@gmail.com>
2019-08-14 23:20:55 -0400

    Eventos y Formularios en HTML y JavaScript
    ahora el usuario podra elegir la cantidad de lineas colocando en un textbox la cantidad deseada.

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,41 +1,13 @@
+var texto = document.getElementById("text_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+//se coloca solo dibujoPorClick sin () al final porque
+//no queremos que dibujoPorClick se ejecute en la linea 3
+//solo lo nombramos para que listener sepa que funcion ejecutara.
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf, xi, yf;
-
-for (l=0; l < lineas; l++)
-{
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  dibujarLinea("#AAF", 0, yi, xf, 300);
-  console.log("lineas " + l);
-}
-/* Tarea de Freddy Vega en Platzi*/
-
-for (l=0; l < lineas; l++)
-{
-  xi = 10 * l;
-  yf = 10 * (l + 1);
-  dibujarLinea("#AAF", xi, 0, 300, yf);
-}
-
-for (l=0; l < lineas; l++)
-{
-  yi = 10 * l;
-  xf = 10 * (30 - l);
-  dibujarLinea("#AAF", 0, yi, xf, 0);
-}
-
-for (l=0; l < lineas; l++)
-{
-  xi = 10 * l;
-  yf = 10 * (30 - l);
-  dibujarLinea("#AAF", xi, 300, 300, yf);
-}
-
-dibujarLinea("#AFA", 1, 1, 1, 299);
-dibujarLinea("#AFA", 1, 300, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -47,6 +19,46 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.closePath(); /* Funcion que cierra el dibujo*/
 }
 
+function dibujoPorClick()
+{
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf, xi, yf;
+  var espacio = ancho / lineas;
 
+  for (l=0; l < lineas; l++)
+  {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea("#AAF", 0, yi, xf, 300);
+    console.log("lineas " + l);
+  }
+  /* Tarea de Freddy Vega en Platzi*/
 
- 
+  for (l=0; l < lineas; l++)
+  {
+    xi = espacio * l;
+    yf = espacio * (l + 1);
+    dibujarLinea("#AAF", xi, 0, 300, yf);
+  }
+
+  // a partir de aqui no funciona las lineas.
+  /*for (l=0; l < lineas; l++)
+  {
+    yi = espacio * l;
+    xf = espacio * (30 - l);
+    dibujarLinea("#AAF", 0, yi, xf, 0);
+  }
+
+  for (l=0; l < lineas; l++)
+  {
+    xi = espacio * l;
+    yf = espacio * (30 - l);
+    dibujarLinea("#AAF", xi, 300, 300, yf);
+  }*/
+
+  dibujarLinea("#AFA", 1, 1, 1, 299);
+  dibujarLinea("#AFA", 1, 299, 299, 299);
+  dibujarLinea("#AFA", 1, 1, 299, 1);
+  dibujarLinea("#AFA", 299, 0, 299, 299);
+}
