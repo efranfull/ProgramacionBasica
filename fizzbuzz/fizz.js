4efc57f710366f55b2b0373fc91c85febb98efb8
Ramon Guzman <efranfull@gmail.com>
2020-02-04 19:50:15 -0400

    División, módulo y residuo Caso 2
    Este es el caso dos donde agregamos if(i%3 != 0 && i%5 !=0), si se cumple el 3 y el 5 entoces muestrame ambas condiciones if.

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -1,16 +1,10 @@
 var numeros = 100;
-//Caso 1 para mostrar el FizzBuzz en lo numero devisibles entre 3 y 5.
-
-var divisible = false;
-
 
 for(var i = 1; i <= 100; i++)
 {
-  divisible = false;  // agregado caso 1
   if(i % 3 == 0)
   {
     document.write("Fizz");
-    divisible = true;   // agregado caso 1
   }
   if (i % 5 == 0) /* aqui podria usar else if y funciona pero, en este
   caso cuando llegara al 15 no me podria FizzBuzz, por ende quitamos
@@ -18,9 +12,8 @@ for(var i = 1; i <= 100; i++)
   un codigo independiente del if de arriba.*/
   {
     document.write("Buzz");
-    divisible = true;   //agregado caso 1
   }
-  if(!divisible) // ! = Sino es divisible
+  if(i%3 != 0 && i%5 !=0) // agregado caso 2
   {
     document.write(i);
   }
