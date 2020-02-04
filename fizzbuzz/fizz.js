7b2591a8d218211a59129b20a608fefbfb1940ec
Ramon Guzman <efranfull@gmail.com>
2020-02-04 19:41:02 -0400

    División, módulo y residuo Caso 1
    Este es el verdero caso uno, al anterior le puse caso 1 en el titulo por error. En este caso lo hicimos con la variable de nombre divisible y con el if en vez de else if, cuando paso por ambas condiciones imprimio fizz y el buzz juntos.

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -1,10 +1,16 @@
 var numeros = 100;
+//Caso 1 para mostrar el FizzBuzz en lo numero devisibles entre 3 y 5.
+
+var divisible = false;
+
 
 for(var i = 1; i <= 100; i++)
 {
+  divisible = false;  // agregado caso 1
   if(i % 3 == 0)
   {
     document.write("Fizz");
+    divisible = true;   // agregado caso 1
   }
   if (i % 5 == 0) /* aqui podria usar else if y funciona pero, en este
   caso cuando llegara al 15 no me podria FizzBuzz, por ende quitamos
@@ -12,8 +18,9 @@ for(var i = 1; i <= 100; i++)
   un codigo independiente del if de arriba.*/
   {
     document.write("Buzz");
+    divisible = true;   //agregado caso 1
   }
-  else
+  if(!divisible) // ! = Sino es divisible
   {
     document.write(i);
   }
